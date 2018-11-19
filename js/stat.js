43da0fd7d39fb123f42daa15f5a1522a72b9965b
person0b <binaryperson@yandex.ru>
2018-11-19 14:23:15 +0300

    Adds colors to constants

--- js/stat.js
+++ js/stat.js
@@ -5,13 +5,18 @@ var CLOUD_Y = 10;
 var CLOUD_HEIGHT = 270;
 var CLOUD_WIDTH = 420;
 var CLOUD_GAP = 10;
+var CLOUD_COLOR = '#fff';
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var BAR_GAP = 50;
+var BAR_YOUR_COLOR = 'rgba(255, 0, 0, 1)';
 var FONT = '16px PT Mono';
 var BASELINE = 'hanging';
 var FONT_GAP = 16;
 var TEXT = ['Ура вы победили!', 'Список результатов:'];
+var TEXT_COLOR = '#000';
+
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
@@ -46,17 +51,17 @@ window.renderStatistics = function (ctx, names, times) {
   };
 
   var getBarColor = function (player) {
-    return player === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(230, ' + (Math.random() * 100).toString() + '%, 50%)';
+    return player === 'Вы' ? BAR_YOUR_COLOR : 'hsl(230, ' + (Math.random() * 100).toString() + '%, 50%)';
   };
 
-  renderCloud(CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, SHADOW_COLOR);
+  renderCloud(CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
   ctx.font = FONT;
   ctx.textBaseline = BASELINE;
 
   for (var i = 0; i < TEXT.length; i++) {
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = TEXT_COLOR;
     ctx.fillText(TEXT[i], CLOUD_X + CLOUD_GAP * 2, CLOUD_Y + CLOUD_GAP * 1.5 + FONT_GAP * i);
   }
 
@@ -77,7 +82,7 @@ window.renderStatistics = function (ctx, names, times) {
       ctx.fillText(times[p], getBarX(p), timeY);
       ctx.fillStyle = getBarColor(names[p]);
       ctx.fillRect(getBarX(p), barY, BAR_WIDTH, barHeight);
-      ctx.fillStyle = '#000';
+      ctx.fillStyle = TEXT_COLOR;
     }
   }
 };
